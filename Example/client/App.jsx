e50532c58b5e11468e1232bf4ab79f1449faab6f
ychang49265 <yc1135@nyu.edu>
2022-06-30 20:02:40 -0400

    accepted incoming changes

--- Example/client/App.jsx
+++ Example/client/App.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { useState, useEffect } from "react";
-// import { useEffect } from "react/cjs/react.production.min";
-import ChatMessage from "./chatMessage.jsx";
+import React, { useState, useEffect } from "react";
+import ChatMessage from "./ChatMessage.jsx";
 
 
 const ws = new WebSocket('ws://localhost:3001');
 
 // whenever client connects to homepage,
-    // ws.addEventListener('open', () => {
-    // console.log('Websocket connection has opened.');
-    // ws.send('open');
+ws.addEventListener('open', () => {
+    console.log('Websocket connection has opened.');
+    ws.send('I connected! Is the back-end able to see this?');
+})
+
+// ws.onmessage = (message) => {
+//     console.log('line 15: onmessage in front end')
+//     console.log("messageFromBack:", message.data);
+//     console.log(message);
+// }
+
+// ws.addEventListener('message', (message) => {
+//     console.log("message.data:", message.data)
 // })
+
 
 const App = () => {
 
@@ -32,55 +41,57 @@ const App = () => {
 // ws.on('error', function error(){
 //     ws.send('error')
 // })
-    
+
     const [messageBoard, setMessageBoard] = useState([]);
     const [message, setMessage] = useState('');
-        
+    const [backMessage, setBackMessage] = useState('');
+
     function handleInputChange(e) {
         setMessage(e.target.value);
     }
-    console.log(`%c ${message}`, 'color: green')
-
-
-    function handleSubmit(e){
-        ws.send('send message', { message })
-        // e.preventDefault()
-        // setMessageBoard([...messageBoard, message]);
+    // console.log(`%c ${message}`, 'color: green')
+    function handleSubmit(){
+        console.log('button clicked');
         
-        // setMessage('');
+        // setMessageBoard([...messageBoard, message]);
+        // console.log('message', message)
+        ws.send(message)
+        setMessage('');
     }
     
-    useEffect(() => {
-        ws.addEventListener('receive message', (message) => {
-
-        })
-    }, [ws])
-   
-
-
-    console.log(`%c ${messageBoard}`, 'color: blue')
+    // console.log(`%c Message Board: ${messageBoard}`, 'color: blue')
     const messageDisplay = []
 
-    messageBoard.map((el, i) => {
-        messageDisplay.push(<ChatMessage message={el.key} key = {i}/>)
-       
-    })
-    console.log(messageDisplay)
+    // messageBoard.map((el, i) => {
+    //     messageDisplay.push(<ChatMessage message={el} key={i}/>)
+    // })
+    ws.onmessage = (messageBack) => { 
+        // console.log('line 15: onmessage in front end')
+        console.log("messageFromBack:", messageBack.data);
+        // console.log(messageBack);
+        setBackMessage(JSON.parse(messageBack.data));
+    }
+
+    // useEffect(() => {
+    //     console.log('Effect used')
+    // }, [backMessage])   
+
+    // console.log('message Display: ', messageDisplay)
+    // console.log('backMessage: ', backMessage);
     return(
         <>
-        
-            
             <input
-            name='newMessage'
-            type='text'
-            value={message}
-            onChange={handleInputChange}
+                name='newMessage'
+                type='text'
+                value={message}
+                onChange={handleInputChange}
             >
             </input>
-            <button type="submit" onClick={handleSubmit}>Send Message</button>
-        
+            <button type='submit' onClick={() => handleSubmit()}>Send Message</button>
+            <ChatMessage
+                messageChat={backMessage}
+            />
        <div>
-        {messageDisplay}
        </div>
         </>
     )
--- Example/client/chatMessage.jsx
+++ Example/client/chatMessage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useState } from "react";
 
 
-function chatMessage(message) {
-    console.log(`%c ${message}`, 'color: green')
+function ChatMessage({ messageChat }) { //use destructuring
+    // console.log('Message in ChatMessage ', message)
     return (
         <>
-        <p>{message}</p>
+        <p>{`ID: ${messageChat.id} ${messageChat.message}`}</p>
         </>
     )
 }
 
-export default chatMessage
+export default ChatMessage;
