e4fb4d1be3c1c11fe9f2177bf2d2f968b47f64c3
fraisai <r.uddin292@gmail.com>
2022-07-13 20:12:53 -0400

    added index.js file in lib folder

---
+++ lib/src/components/Box.tsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import { DefaultProps, PolymorphicComponentProps } from '@mantine/styles';
+// interface _BoxProps extends DefaultProps {
+// }
+// export declare type BoxProps<C> = PolymorphicComponentProps<C, _BoxProps>;
+// declare type BoxComponent = (<C = 'div'>(props: BoxProps<C>) => React.ReactElement) & {
+//     displayName?: string;
+// };
+// export declare const Box: BoxComponent;
+// export {};
+// //# sourceMappingURL=Box.d.ts.map
+
+
+
+// // index.d.ts
+
+// export { Box } from './Box';
+// export type { BoxProps } from './Box';
+// //# sourceMappingURL=index.d.ts.map
