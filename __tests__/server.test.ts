c284fefbbde96b40672607ee8d9404b4bbf01ca5
fraisai <r.uddin292@gmail.com>
2022-07-15 20:07:22 -0400

    configured Jest to implement testing for typescript and created server.test.ts to test SignalingChannel class

---
+++ __tests__/server.test.ts
@@ -0,0 +1,20 @@
+import SignalingChannel = require('../lib/server/server');
+
+// check to make sure npm test is working
+describe('is working', () => {
+  it('show work', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('SignalingChannel initializeConnection()', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  beforeAll(() => {
+    // Start websocket server
+  });
+
+  afterAll(() => {
+    // Close websocket server
+  });
+
+});
