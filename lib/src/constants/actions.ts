d20f71f404c0921c2e2785a154e609f29d10a2e0
Raisa <9156403+fraisai@users.noreply.github.com>
2022-09-12 19:09:10 -0400

    Update actions.ts

--- lib/src/constants/actions.ts
+++ lib/src/constants/actions.ts
@@ -12,6 +12,22 @@ type actionType = {
 /**
  * @desc actions that will be used by SignalingChannel and Socket component for being able to filter and identify data passed through websocket message event
 */
+
+// consider using enums 
+// enums are like objects where the keys are fixed at compile time, so TypeScript can check that the given key actually exists when you access it.
+/*
+const enum actions {
+    CONNECTION: 'CONNECTION',
+    OFFER: 'OFFER',
+    ANSWER: 'ANSWER',
+    LOGIN: 'LOGIN',
+    ICECANDIDATE: 'ICECANDIDATE',
+    LEAVE: 'LEAVE',
+    CREATE_ROOM: 'CREATE_ROOM',
+    JOIN_ROOM: 'JOIN_ROOM'
+}
+*/
+
 const actions: actionType = {
   CONNECTION: 'CONNECTION',
   OFFER: 'OFFER',
