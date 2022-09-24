c4775e53be3819150420f4f82fff7f2b6093b8b4
fraisai <r.uddin292@gmail.com>
2022-09-24 13:30:40 -0400

    updated tsconfig.json to allow noFallthroughCasesInSwitch

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,9 @@
-// import { Config } from '@jest/types';
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   }
-
 };
 
 export default jestConfig;
