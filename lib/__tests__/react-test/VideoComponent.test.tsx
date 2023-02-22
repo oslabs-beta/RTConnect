fea97a0af9fee51ec8866dd99fa280b47a02baed
fraisai <r.uddin292@gmail.com>
2023-02-21 23:05:29 -0500

    react testing library

--- lib/__tests__/react-test/VideoComponent.test.tsx
+++ lib/__tests__/react-test/VideoComponent.test.tsx
@@ -1,24 +1,23 @@
-/**
- * @jest-environment jsdom
- */
+/** @jest-environment jsdom */
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import VideoComponent from '../../src/components/VideoComponent';
-import React from 'react';
-
 
 describe('VideoComponent', () => {
-  it('render video components', () => {
-    const localVideo = null;
+  const localVideo = null;
   
-    const options = { controls: true, style: { width: '640px', height: '360px'}}; 
-    const { controls, style } = options;
+  const options = { controls: true, style: { width: '640px', height: '360px'}}; 
+  const { controls, style } = options;
 
-    const { container } = render(<VideoComponent video = {localVideo} mediaOptions = {options}/>);
+  it('controls should be true', () => {
     expect(controls).toBe(true);
+    expect(style).toStrictEqual({ width: '640px', height: '360px'});
+  });
+
+  it('render video', () => {
+    const { container } = render(<VideoComponent video = {localVideo} mediaOptions = {options}/>);
+    expect(container).toMatchSnapshot();
   });
 
 });
