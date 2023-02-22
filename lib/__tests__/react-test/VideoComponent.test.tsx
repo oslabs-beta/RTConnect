c91b8f9afce4f6e1bbedd010a5ff2b09de5beaab
fraisai <r.uddin292@gmail.com>
2023-02-21 22:28:08 -0500

    added test for react testing suite

--- lib/__tests__/react-test/VideoComponent.test.tsx
+++ lib/__tests__/react-test/VideoComponent.test.tsx
@@ -2,20 +2,23 @@
  * @jest-environment jsdom
  */
 
+/**
+ * @jest-environment jsdom
+ */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+
 import { render, screen } from '@testing-library/react';
 import VideoComponent from '../../src/components/VideoComponent';
-import React, { useRef } from 'react';
+import React from 'react';
 
 
 describe('VideoComponent', () => {
   it('render video components', () => {
-    // const localVideo = useRef<HTMLVideoElement>(null!);
     const options = { controls: true, style: { width: '640px', height: '360px'}};
-
     const { container } = render(<VideoComponent video = {null} mediaOptions = {options}/>);
-    // expect(container).toMatchSnapshot();
+    expect(options.controls).toBe(true);
   });
 
 });
