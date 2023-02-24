bd6df8a3b8b6513dc7dffbd79064e09ebd0b1dd0
fraisai <r.uddin292@gmail.com>
2023-02-24 14:49:19 -0500

    added testing and data-testid attributes to allow for easier testing

--- lib/__tests__/react-test/VideoCall.unit.test.tsx
+++ lib/__tests__/react-test/VideoCall.unit.test.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /** @jest-environment jsdom */
 
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import VideoCall from '../../src/components/VideoCall';
+
+afterAll(cleanup);
 
 describe('VideoCall', () => {
   const Url = 'ws://localhost:3000';
@@ -16,10 +19,20 @@ describe('VideoCall', () => {
 
   const { container } = render(<VideoCall URL={Url} mediaOptions={options}/>);
 
-  it('Check Submit Username button', () => {
+  it('Check buttons in VideoCall.tsx', () => {
 
     const submitBtn = screen.getByTestId('submit-username-btn');
     expect(submitBtn.innerHTML).toBe('Submit Username');
+
+    const shareBtn = screen.getByTestId('share-screen-btn');
+    expect(shareBtn.innerHTML).toBe('Share Screen');
+
+    const endBtn = screen.getByTestId('end-call-btn');
+    expect(endBtn.innerHTML).toBe('End Call');
+
+    const callBtn = screen.getByTestId('call-btn');
+    expect(callBtn.innerHTML).toBe('Call');
+
     // fireEvent.click(submitBtn);
     // expect()
   });
