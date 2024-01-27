c3f3a52f166c5f8e450aba65c5709ae1e01151c8
fraisai <r.uddin292@gmail.com>
2024-01-26 22:16:07 -0500

    landing page
    Co-authored-by: YooJin Chang yc1135@nyu.edu
    Co-authored-by: Louis Disen disenelouis@gmail.com

--- rtconnect-landing/client/containers/Home.jsx
+++ rtconnect-landing/client/containers/Home.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SectionOne from '../components/sections/SectionOne.jsx';
 import SectionTwo from '../components/sections/SectionTwo.jsx';
 import SectionThree from '../components/sections/SectionThree.jsx';
-import Team from '../components/Team.jsx'
-import Footer from '../components/Footer.jsx'
-import RTNavBar from '../components/RTNavbar.jsx'
-import RTHeader from '../components/sections/RTHeader.jsx'
+import Team from '../components/Team.jsx';
+import Footer from '../components/Footer.jsx';
+import RTNavBar from '../components/RTNavbar.jsx';
+import RTHeader from '../components/sections/RTHeader.jsx';
 
 const Home = () => {
 
   return (
     <div className="home">
       {/* <div className=' font-body bg-gradient-to-r from-indigo-900 via-sky-800 to-cyan-500'> */}
-        <div class=" font-body bg-zinc-100">
+      <div className="font-body bg-zinc-100">
        
         <RTNavBar />
         <SectionOne />
@@ -38,9 +38,9 @@ const Home = () => {
         /> */}
 
         <Footer />
-        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
